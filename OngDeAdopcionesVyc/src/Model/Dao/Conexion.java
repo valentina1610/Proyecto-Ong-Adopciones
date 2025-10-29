d924c7bed5743d9d52e44da02c773a1d7bb5b866
yuli <giulianamanzo1199@gmail.com>
2025-10-29 20:35:08 -0300

    .

--- OngDeAdopcionesVyc/src/Model/Dao/Conexion.java
+++ OngDeAdopcionesVyc/src/Model/Dao/Conexion.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class Conexion {
     // Datos de conexión
-    private static final String URL = "jdbc:mysql://IP_DEL_SERVIDOR:3306/refugiodb";
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/refugiodb";
     private static final String USER = "root";
     private static final String PASSWORD = "";
 
